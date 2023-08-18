c854906d4e5210a23708e2cdb394fdaf544d11e6
Yuvraj Kumar <img_2021066@gmail.com>
2023-08-18 14:39:54 +0530

    Project Initial Commit

---
+++ app/src/main/java/com/yuvraj/visionai/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yuvraj.visionai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
