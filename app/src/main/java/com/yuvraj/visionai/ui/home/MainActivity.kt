d9efddf0d69377940e483443cf7f8e0479788410
Yuvraj Kumar <img_2021066@gmail.com>
2023-08-20 16:04:09 +0530

    Created Fragments and added navigation between them using nav controller

--- app/src/main/java/com/yuvraj/visionai/ui/home/MainActivity.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/MainActivity.kt
@@ -2,15 +2,23 @@ package com.yuvraj.visionai.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.yuvraj.visionai.R
+import com.yuvraj.visionai.databinding.UiHomeActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: UiHomeActivityMainBinding? = null
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initviews()
+        setContentView(binding?.root)
     }
 
     fun initviews() {
-
+        binding = UiHomeActivityMainBinding.inflate(layoutInflater,null,false)
+        navHostFragment = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment)
     }
 }
