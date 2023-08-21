3b59629ba1b59a61984856d85acee68438be82a3
Yuvraj Kumar <img_2021066@gmail.com>
2023-08-22 00:44:55 +0530

    Added Speech Recognizer

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/EyeTestingFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/EyeTestingFragment.kt
@@ -1,18 +1,31 @@
 package com.yuvraj.visionai.ui.home.fragments
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.yuvraj.visionai.R
 import com.yuvraj.visionai.databinding.FragmentHomeEyeTestingBinding
-
 
 /**
  * A simple [Fragment] subclass.
  * Use the [EyeTestingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EyeTestingFragment : Fragment(R.layout.fragment_home_eye_testing) {
+class EyeTestingFragment : Fragment(R.layout.fragment_home_eye_testing), RecognitionListener {
+
+    private val permission = 100
+    private lateinit var speech: SpeechRecognizer
+    private lateinit var recognizerIntent: Intent
+    private var logTag = "VoiceRecognitionActivity"
 
     private var _binding: FragmentHomeEyeTestingBinding? = null
     private val binding get() = _binding!!
@@ -30,12 +43,121 @@ class EyeTestingFragment : Fragment(R.layout.fragment_home_eye_testing) {
 
     private fun initViews(view: View) {
         _binding = FragmentHomeEyeTestingBinding.bind(view)
+
+        binding.progressBar.visibility = View.VISIBLE
+        speech = SpeechRecognizer.createSpeechRecognizer(requireContext())
+        Log.i(logTag, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(requireContext()))
+        speech.setRecognitionListener(this)
+        recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "US-en")
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
+
     }
 
     private fun clickableViews() {
 
         binding.apply {
-
+            toggleButton.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.progressBar.isIndeterminate = true
+                    ActivityCompat.requestPermissions(requireActivity(),
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        permission)
+                } else {
+                    binding.progressBar.isIndeterminate = false
+                    binding.progressBar.visibility = View.VISIBLE
+                    speech.stopListening()
+                }
+            }
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permission -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager
+                    .PERMISSION_GRANTED) {
+                speech.startListening(recognizerIntent)
+            } else {
+                Toast.makeText(requireActivity(),
+                    "Permission Denied!",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        speech.destroy()
+        Log.i(logTag, "destroy")
+    }
+
+    override fun onReadyForSpeech(params: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRmsChanged(rmsdB: Float) {
+        binding.progressBar.progress = rmsdB.toInt()
+    }
+
+    override fun onBufferReceived(buffer: ByteArray?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPartialResults(partialResults: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEvent(eventType: Int, params: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBeginningOfSpeech() {
+        Log.i(logTag, "onBeginningOfSpeech")
+        binding.progressBar.isIndeterminate = false
+        binding.progressBar.max = 10
+    }
+
+    override fun onEndOfSpeech() {
+        binding.progressBar.isIndeterminate = true
+        binding.toggleButton.isChecked = false
+    }
+    override fun onError(error: Int) {
+        val errorMessage: String = getErrorText(error)
+        Log.d(logTag, "FAILED $errorMessage")
+        binding.textViewSpeechToText.text = errorMessage
+        binding.toggleButton.isChecked = false
+    }
+    private fun getErrorText(error: Int): String {
+        var message = ""
+        message = when (error) {
+            SpeechRecognizer.ERROR_AUDIO -> "Audio recording error"
+            SpeechRecognizer.ERROR_CLIENT -> "Client side error"
+            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "Insufficient permissions"
+            SpeechRecognizer.ERROR_NETWORK -> "Network error"
+            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "Network timeout"
+            SpeechRecognizer.ERROR_NO_MATCH -> "No match"
+            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "RecognitionService busy"
+            SpeechRecognizer.ERROR_SERVER -> "error from server"
+            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "No speech input"
+            else -> "Didn't understand, please try again."
+        }
+        return message
+    }
+    override fun onResults(results: Bundle?) {
+        Log.i(logTag, "onResults")
+        val matches = results!!.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+        var text = ""
+        if (matches != null) {
+            for (result in matches) text = """
+          $result
+          """.trimIndent()
+        }
+        binding.textViewSpeechToText.text = text
     }
 }
