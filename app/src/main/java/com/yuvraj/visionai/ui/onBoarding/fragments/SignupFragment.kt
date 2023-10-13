c4bed9db97692713340379a4df750eb34d388f7b
Yuvraj Kumar <img_2021066@gmail.com>
2023-10-13 22:05:38 +0530

    Updated Ui for SignUp Page

--- app/src/main/java/com/yuvraj/visionai/ui/onBoarding/fragments/SignupFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/onBoarding/fragments/SignupFragment.kt
@@ -5,9 +5,20 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.yuvraj.visionai.R
 import com.yuvraj.visionai.databinding.FragmentOnBoardingSignupBinding
+import com.yuvraj.visionai.ui.home.MainActivity
 import com.yuvraj.visionai.utils.helpers.Validations
 
 
@@ -20,8 +31,9 @@ class SignupFragment : Fragment(R.layout.fragment_on_boarding_signup) {
     private var _binding: FragmentOnBoardingSignupBinding? = null
     private val binding get() = _binding!!
 
-//    lateinit var mGoogleSignInClient: GoogleSignInClient
-//    private lateinit var firebaseAuth: FirebaseAuth
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code: Int = 123
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,22 +48,22 @@ class SignupFragment : Fragment(R.layout.fragment_on_boarding_signup) {
 //        showSoftKeyboard(requireActivity(),binding.etName)
         clickableViews(binding.etName,binding.etEmail,binding.etPassword,binding.etConfirmPassword)
 
-//        FirebaseApp.initializeApp(this.requireActivity())
+        FirebaseApp.initializeApp(this.requireActivity())
 
-//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestIdToken(getString(R.string.default_web_client_id))
-//            .requestEmail()
-//            .build()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 //
-//        mGoogleSignInClient = GoogleSignIn.getClient(this.requireActivity(), gso)
-//        firebaseAuth = FirebaseAuth.getInstance()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
 
-//    private fun signInGoogle() {
-//        val signInIntent: Intent = mGoogleSignInClient.signInIntent
-//        startActivity(signInIntent)
-//    }
+    private fun signInGoogle() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivity(signInIntent)
+    }
 
 
     private fun clickableViews(Name : EditText, Email : EditText, Password : EditText, ConfirmPassword : EditText) {
@@ -89,10 +101,11 @@ class SignupFragment : Fragment(R.layout.fragment_on_boarding_signup) {
                 }
             }
 
-//            googleButton.setOnClickListener() {view : View? ->
-//                signInGoogle()
-//                findNavController().navigate(R.id.action_onBoardingSignUpFragment_to_onBoardingAboutFrag)
-//            }
+            btnGoogleSignIn.setOnClickListener() {view : View? ->
+                signInGoogle()
+                Toast.makeText(requireActivity(), "Logging In", Toast.LENGTH_SHORT).show()
+                // findNavController().navigate(R.id.action_signupFragment_to_detailsFragment)
+            }
 
             tvLoginHere.setOnClickListener {
                 findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
@@ -112,5 +125,50 @@ class SignupFragment : Fragment(R.layout.fragment_on_boarding_signup) {
         _binding = FragmentOnBoardingSignupBinding.bind(view)
 //        ConstantsFunctions.setStatusBarTransparent(requireActivity(), false)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Req_Code) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(requireActivity(), e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // this is where we update the UI after Google signin takes place
+    private fun UpdateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+//                SavedPreferences.setEmail(requireActivity(), account.email.toString())
+//                SavedPreferences.setUsername(requireActivity(), account.displayName.toString())
+                val intent = Intent(requireActivity(), MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (GoogleSignIn.getLastSignedInAccount(requireContext()) != null) {
+            startActivity(
+                Intent(
+                    requireActivity(), MainActivity::class.java
+                )
+            )
+            requireActivity().finish()
+        }
     }
 }
