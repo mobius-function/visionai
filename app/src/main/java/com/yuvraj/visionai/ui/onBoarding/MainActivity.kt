38e6568644353ca0725ca5a0d7e6b83b5dfd3d24
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-02-19 19:09:06 +0530

    Added functionality for in-app updates

--- app/src/main/java/com/yuvraj/visionai/ui/onBoarding/MainActivity.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/onBoarding/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.yuvraj.visionai.ui.onBoarding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.yuvraj.visionai.R
 import com.yuvraj.visionai.databinding.UiOnboardingActivityMainBinding
+import com.yuvraj.visionai.service.autoUpdater.InAppUpdate
+
 
 class MainActivity: AppCompatActivity() {
 
     private var binding: UiOnboardingActivityMainBinding? = null
     private lateinit var navHostFragment: NavHostFragment
+
+    //In-App Update manager
+    private var inAppUpdate: InAppUpdate? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +26,25 @@ class MainActivity: AppCompatActivity() {
     fun initviews() {
         binding = UiOnboardingActivityMainBinding.inflate(layoutInflater,null,false)
         navHostFragment = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment)
+
+        //In-App Update Initializer
+        inAppUpdate = InAppUpdate(this@MainActivity)
+        inAppUpdate!!.checkForAppUpdate()
+    }
+
+    // Override methods for In-App Update
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        inAppUpdate!!.onActivityResult(requestCode, resultCode)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        inAppUpdate!!.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        inAppUpdate!!.onDestroy()
     }
 }
