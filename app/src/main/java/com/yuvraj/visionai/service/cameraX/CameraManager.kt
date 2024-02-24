1518d2b747ea710c6342af0cd4b4f697bfe7fde4
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-02-25 00:49:16 +0530

    code cleaning

--- app/src/main/java/com/yuvraj/visionai/service/cameraX/CameraManager.kt
+++ app/src/main/java/com/yuvraj/visionai/service/cameraX/CameraManager.kt
@@ -1,7 +1,9 @@
 package com.yuvraj.visionai.service.cameraX
 
 import android.content.Context
+import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.util.Log
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
@@ -9,11 +11,14 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.google.mlkit.vision.face.Face
 import com.yuvraj.visionai.service.faceDetection.FaceContourDetectionProcessor
-import com.yuvraj.visionai.service.faceDetection.FaceStatus
+import com.yuvraj.visionai.model.FaceStatus
+import com.yuvraj.visionai.utils.DebugTags
+import com.yuvraj.visionai.utils.DebugTags.CAMERA_X_BASICS
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -89,7 +94,7 @@ class CameraManager(
                 finderView.surfaceProvider
             )
         } catch (e: Exception) {
-            Log.e(TAG, "Use case binding failed", e)
+            Log.e(CAMERA_X_BASICS, "Use case binding failed", e)
         }
     }
 
@@ -102,8 +107,34 @@ class CameraManager(
         startCamera()
     }
 
-    companion object {
-        private const val TAG = "CameraXBasic"
-    }
+    fun getCameraDetails(context: Context) {
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
+
+        try {
+            // Get the list of available camera IDs
+            val cameraIds = cameraManager.cameraIdList
+
+            for (cameraId in cameraIds) {
+                // Get the camera characteristics for the specified camera ID
+                val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+
+                // Get the focal lengths array
+                val focalLengths = characteristics.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS)
+                val sensorSize = characteristics.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)
+
+                // Print the focal lengths
+                for (focalLength in focalLengths ?: floatArrayOf()) {
+                    // Update your UI or log the focal lengths as needed
+                    Log.e(DebugTags.DEVICE_INFO,"Camera $cameraId Focal Length: $focalLength\n")
+                    Log.e(DebugTags.DEVICE_INFO,"Camera $cameraId Sensor Size: $sensorSize\n")
+
+
+                }
+            }
+
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
 }
