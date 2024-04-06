dea700960945134d8264cfebfa96675f6e69536f
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-04-06 19:54:46 +0530

    added debug tags

--- app/src/main/java/com/yuvraj/visionai/utils/DebugTags.kt
+++ app/src/main/java/com/yuvraj/visionai/utils/DebugTags.kt
@@ -5,4 +5,5 @@ object DebugTags {
     const val CAMERA_X_BASICS = "CAMERA_X_BASICS"
     const val CAMERA_X = "CAMERA_X"
     const val FACE_DETECTION = "FACE_DETECTION"
+    const val FIREBASE_PUSH_NOTIFICATION = "FIREBASE_PUSH_NOTIFICATION"
 }
