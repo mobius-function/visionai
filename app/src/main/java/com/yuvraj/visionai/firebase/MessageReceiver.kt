7023a400a7a53f3eb8ec85196ab0cbd10e42940a
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-04-06 19:53:53 +0530

    added fcm dependencies

---
+++ app/src/main/java/com/yuvraj/visionai/firebase/MessageReceiver.kt
@@ -0,0 +1,94 @@
+package com.yuvraj.visionai.firebase
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.yuvraj.visionai.R
+import com.yuvraj.visionai.ui.home.MainActivity
+import com.yuvraj.visionai.utils.DebugTags.FIREBASE_PUSH_NOTIFICATION
+import java.io.IOException
+import java.net.URL
+
+class MessagingService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        if (message.data.size > 0) {
+            Log.d(FIREBASE_PUSH_NOTIFICATION, "Message Data payload: " + message.data)
+        }
+        if (message.notification != null) {
+            sendNotification(
+                message.notification!!.body, message.notification!!.title, message.notification!!
+                    .imageUrl
+            )
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(FIREBASE_PUSH_NOTIFICATION, "Refreshed token: $token")
+    }
+
+    private fun sendNotification(messageBody: String?, title: String?, imgUrl: Uri?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        var bmp: Bitmap? = null
+        Log.d(FIREBASE_PUSH_NOTIFICATION, "sendNotification: " + imgUrl.toString())
+        try {
+            val `in` = URL(imgUrl.toString()).openStream()
+            bmp = BitmapFactory.decodeStream(`in`)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder: NotificationCompat.Builder =
+            NotificationCompat.Builder(this, channelId)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bmp))
+                .setPriority(Notification.PRIORITY_DEFAULT)
+
+//        val notificationBuilder: NotificationCompat.Builder =
+//            NotificationCompat.Builder(this, channelId)
+//                .setSmallIcon(R.drawable.ic_notif)
+//                .setContentTitle(title)
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent)
+//                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bmp))
+//                .setPriority(Notification.PRIORITY_DEFAULT)
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
