e5a14daf27cd0317f4451f317ad8ebaacc41bb01
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-04-12 14:25:37 +0530

    Changed UI for Notification Fragment

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/NotificationsFragment.kt
@@ -1,6 +1,7 @@
 package com.yuvraj.visionai.ui.home.fragments
 
 import android.app.TimePickerDialog
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
@@ -12,53 +13,6 @@ class NotificationsFragment : Fragment(R.layout.fragment_home_notifications) {
 
     private var _binding: FragmentHomeNotificationsBinding? = null
     private val binding get() = _binding!!
-
-    // listener which is triggered when the
-    // time is picked from the time picker dialog
-    private val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
-        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> // logic to properly handle
-            // the picked timings by user
-            val formattedTime: String = when {
-                hourOfDay == 0 -> {
-                    if (minute < 10) {
-                        "${hourOfDay + 12} : 0${minute} am"
-                    } else {
-                        "${hourOfDay + 12} : $minute am"
-                    }
-                }
-
-                hourOfDay > 12 -> {
-                    if (minute < 10) {
-                        "${hourOfDay - 12} : 0$minute pm"
-                    } else {
-                        "${hourOfDay - 12} : $minute pm"
-                    }
-                }
-
-                hourOfDay == 12 -> {
-                    if (minute < 10) {
-                        "$hourOfDay : 0$minute pm"
-                    } else {
-                        "$hourOfDay : $minute pm"
-                    }
-                }
-
-                else -> {
-                    if (minute < 10) {
-                        "$hourOfDay : $minute am"
-                    } else {
-                        "$hourOfDay : $minute am"
-                    }
-                }
-            }
-
-            binding.tvTimeRegularReminder.text = formattedTime
-        }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -73,30 +27,18 @@ class NotificationsFragment : Fragment(R.layout.fragment_home_notifications) {
 
     private fun clickableViews() {
         binding.apply {
-            tvTimeRegularReminder.setOnClickListener {
-                val timePickerDialog = TimePickerDialog(
-                    requireContext(),
-                    timePickerDialogListener,
-                    12,
-                    10,
-                    false
-                )
-                timePickerDialog.show()
-            }
 
-            tbRegularReminder.setOnCheckedChangeListener() { _, isChecked ->
+            tbRegularReminder.setOnCheckedChangeListener { _, isChecked ->
                 if (isChecked) {
-                    tvTimeRegularReminder.isClickable = true
-                    tvTimeRegularReminder.setTextColor("#FFFFFF".toColorInt())
+                    tvTimeInHours.setTextColor("#FFFFFF".toColorInt())
+                    btnTimeInHoursMinus.backgroundTintList = (ColorStateList.valueOf("#FFFFFF".toColorInt()))
+                    btnTimeInHoursPlus.backgroundTintList = (ColorStateList.valueOf("#FFFFFF".toColorInt()))
                 } else {
-                    tvTimeRegularReminder.isClickable = false
-                    tvTimeRegularReminder.setTextColor("#898989".toColorInt())
+                    tvTimeInHours.setTextColor("#898989".toColorInt())
+                    btnTimeInHoursMinus.backgroundTintList = (ColorStateList.valueOf("#898989".toColorInt()))
+                    btnTimeInHoursPlus.backgroundTintList = (ColorStateList.valueOf("#898989".toColorInt()))
                 }
             }
-
-            // setting the default state of the toggle button (Debugging purpose)
-            tbRegularReminder.isChecked = true
-            tbRegularReminder.isChecked = false
         }
     }
 }
