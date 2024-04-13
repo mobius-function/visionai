f89b702bafe0e550b89b0851bf4ab4b86eae0bb6
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-04-13 15:36:17 +0530

    Added new constants for notification preferences

--- app/src/main/java/com/yuvraj/visionai/utils/Constants.kt
+++ app/src/main/java/com/yuvraj/visionai/utils/Constants.kt
@@ -3,12 +3,14 @@ package com.yuvraj.visionai.utils
 object Constants {
     const val USER_DETAILS = "user_details"
 
-    const val USER_ONBOARDING_COMPLETED = "user_onboarding_completed"
+    const val USER_ONBOARDING_COMPLETED = "user_onboarding_completed"       // Boolean
+
+    const val FIRST_USE_AFTER_LOGIN = "FIRST_USE_AFTER_LOGIN"               // Boolean
 
     const val USER_NAME = "user_name"
     const val USER_EMAIL = "user_email"
-    const val USER_PHONE = "user_phone"
-    const val USER_AGE = "user_age"
+    const val USER_PHONE = "user_phone"                                     // Long
+    const val USER_AGE = "user_age"                                        // Int
 
     // Eye Tests IDs
     const val MYOPIA = "MYOPIA"
@@ -20,8 +22,11 @@ object Constants {
     const val LOGOUT = "LOGOUT"
 
     // Settings Preferences
-    const val EYE_TEST_REMINDER = "EYE_TEST_REMINDER"
-    const val EYE_TEST_REMINDER_TIME = "EYE_TEST_REMINDER_TIME"
-    const val REGULAR_REMINDER = "REGULAR_REMINDER"
-    const val REGULAR_REMINDER_TIME = "REGULAR_REMINDER_TIME"
+    const val NOTIFICATION_PREFERENCES = "NOTIFICATION_PREFERENCES"
+
+    const val EYE_TEST_REMINDER = "EYE_TEST_REMINDER"                    // Boolean
+    const val EYE_TEST_REMINDER_TIME = "EYE_TEST_REMINDER_TIME"         // Long
+
+    const val REGULAR_REMINDER = "REGULAR_REMINDER"                     // Boolean
+    const val REGULAR_REMINDER_TIME = "REGULAR_REMINDER_TIME"           // Long
 }
