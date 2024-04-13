5689ef7a2b67a2324635b266314dc413b7c710ff
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-04-13 15:38:19 +0530

    Completed regular reminders

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/NotificationsFragment.kt
@@ -1,13 +1,19 @@
 package com.yuvraj.visionai.ui.home.fragments
 
+import android.annotation.SuppressLint
 import android.app.TimePickerDialog
 import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.toColorInt
 import com.yuvraj.visionai.R
 import com.yuvraj.visionai.databinding.FragmentHomeNotificationsBinding
+import com.yuvraj.visionai.utils.Constants
+import com.yuvraj.visionai.utils.Constants.REGULAR_REMINDER
+import com.yuvraj.visionai.utils.Constants.EYE_TEST_REMINDER
+import com.yuvraj.visionai.utils.Constants.REGULAR_REMINDER_TIME
 
 class NotificationsFragment : Fragment(R.layout.fragment_home_notifications) {
 
@@ -23,20 +29,109 @@ class NotificationsFragment : Fragment(R.layout.fragment_home_notifications) {
 
     private fun initViews(view: View) {
         _binding = FragmentHomeNotificationsBinding.bind(view)
+
+        val sharedPreferences = requireActivity().getSharedPreferences(
+            Constants.NOTIFICATION_PREFERENCES,
+            AppCompatActivity.MODE_PRIVATE
+        )
+
+        binding.tbEyeTestReminder.isChecked = sharedPreferences.getBoolean(EYE_TEST_REMINDER, true)
+        binding.tbRegularReminder.isChecked = sharedPreferences.getBoolean(REGULAR_REMINDER, false)
+
+        binding.tvTimeInHours.text = (sharedPreferences.getInt(REGULAR_REMINDER_TIME, 2)).toString()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun clickableViews() {
         binding.apply {
+            btnTimeInHoursPlus.setOnClickListener {
+                if (tvTimeInHours.text.toString().toInt() >= 2) {
+                    tvTimeInHours.text = (tvTimeInHours.text.toString().toInt() + 1).toString()
+
+                    val sharedPreferences = requireActivity().getSharedPreferences(
+                        Constants.NOTIFICATION_PREFERENCES,
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    val sharedPreferencesEditor = sharedPreferences.edit()
+
+                    sharedPreferencesEditor.putInt(
+                        REGULAR_REMINDER_TIME,
+                        binding.tvTimeInHours.text.toString().toInt()
+                    )
+                    sharedPreferencesEditor.apply()
+                }
+            }
+
+            btnTimeInHoursMinus.setOnClickListener {
+                if (tvTimeInHours.text.toString().toInt() <= 24) {
+                    tvTimeInHours.text = (tvTimeInHours.text.toString().toInt() - 1).toString()
+
+                    val sharedPreferences = requireActivity().getSharedPreferences(
+                        Constants.NOTIFICATION_PREFERENCES,
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    val sharedPreferencesEditor = sharedPreferences.edit()
+
+                    sharedPreferencesEditor.putInt(
+                        REGULAR_REMINDER_TIME,
+                        binding.tvTimeInHours.text.toString().toInt()
+                    )
+                    sharedPreferencesEditor.apply()
+                }
+            }
+
+
+            tbEyeTestReminder.setOnCheckedChangeListener{_, isChecked ->
+                if (isChecked){
+                    val sharedPreferences = requireActivity().getSharedPreferences(
+                        Constants.NOTIFICATION_PREFERENCES,
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    val sharedPreferencesEditor = sharedPreferences.edit()
+                    sharedPreferencesEditor.putBoolean(EYE_TEST_REMINDER, true)
+                    sharedPreferencesEditor.apply()
+                } else{
+                    val sharedPreferences = requireActivity().getSharedPreferences(
+                        Constants.NOTIFICATION_PREFERENCES,
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    val sharedPreferencesEditor = sharedPreferences.edit()
+                    sharedPreferencesEditor.putBoolean(EYE_TEST_REMINDER, false)
+                    sharedPreferencesEditor.apply()
+                }
+            }
 
             tbRegularReminder.setOnCheckedChangeListener { _, isChecked ->
                 if (isChecked) {
                     tvTimeInHours.setTextColor("#FFFFFF".toColorInt())
                     btnTimeInHoursMinus.backgroundTintList = (ColorStateList.valueOf("#FFFFFF".toColorInt()))
                     btnTimeInHoursPlus.backgroundTintList = (ColorStateList.valueOf("#FFFFFF".toColorInt()))
+
+                    val sharedPreferences = requireActivity().getSharedPreferences(
+                        Constants.NOTIFICATION_PREFERENCES,
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    val sharedPreferencesEditor = sharedPreferences.edit()
+                    sharedPreferencesEditor.putBoolean(REGULAR_REMINDER, true)
+                    sharedPreferencesEditor.apply()
                 } else {
                     tvTimeInHours.setTextColor("#898989".toColorInt())
                     btnTimeInHoursMinus.backgroundTintList = (ColorStateList.valueOf("#898989".toColorInt()))
                     btnTimeInHoursPlus.backgroundTintList = (ColorStateList.valueOf("#898989".toColorInt()))
+
+                    val sharedPreferences = requireActivity().getSharedPreferences(
+                        Constants.NOTIFICATION_PREFERENCES,
+                        AppCompatActivity.MODE_PRIVATE
+                    )
+
+                    val sharedPreferencesEditor = sharedPreferences.edit()
+                    sharedPreferencesEditor.putBoolean(REGULAR_REMINDER, false)
+                    sharedPreferencesEditor.apply()
                 }
             }
         }
