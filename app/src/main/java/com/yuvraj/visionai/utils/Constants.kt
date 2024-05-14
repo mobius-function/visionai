1d326a49ff9a348195682eff90ff3fa67c987342
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-05-14 23:22:19 +0530

    fixed constants

--- app/src/main/java/com/yuvraj/visionai/utils/Constants.kt
+++ app/src/main/java/com/yuvraj/visionai/utils/Constants.kt
@@ -8,29 +8,37 @@ object Constants {
         android.Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
-    // Boolean States
-    const val USER_ONBOARDING_COMPLETED = "USER_ONBOARDING_COMPLETED"
+    val REQUIRED_PERMISSIONS_FOR_CAMERA = arrayOf(
+        android.Manifest.permission.CAMERA
+    )
 
-    const val IS_ALL_PERMISSIONS_GRANTED = "IS_ALL_PERMISSIONS_GRANTED"
-
-    const val FIRST_USE_AFTER_LOGIN = "FIRST_USE_AFTER_LOGIN"
-
-    const val ALL_IN_ONE_EYE_TEST = "ALL_IN_ONE_EYE_TEST"
 
     // Test Results
+    const val EYE_TEST_RESULTS = "EYE_TEST_RESULTS"
+
+    const val LEFT_EYE_PARTIAL_BLINK_COUNTER = "LEFT_EYE_PARTIAL_BLINK_COUNTER"
+    const val RIGHT_EYE_PARTIAL_BLINK_COUNTER = "RIGHT_EYE_PARTIAL_BLINK_COUNTER"
+    const val TOTAL_TIME_SPENT_TESTING = "TOTAL_TIME_SPENT_TESTING"
     const val ALL_IN_ONE_EYE_TEST_RESULTS = "ALL_IN_ONE_EYE_TEST_RESULTS"
     const val MYOPIA_RESULTS = "MYOPIA_RESULTS"
     const val HYPEROPIA_RESULTS = "HYPEROPIA_RESULTS"
     const val ASTIGMATISM_RESULTS = "ASTIGMATISM_RESULTS"
     const val DRY_EYE_RESULTS = "DRY_EYE_RESULTS"
 
+
     // User Details
     const val USER_DETAILS = "USER_DETAILS"
+
+    const val USER_ONBOARDING_COMPLETED = "USER_ONBOARDING_COMPLETED"
+    const val IS_ALL_PERMISSIONS_GRANTED = "IS_ALL_PERMISSIONS_GRANTED"
+    const val FIRST_USE_AFTER_LOGIN = "FIRST_USE_AFTER_LOGIN"
+    const val ALL_IN_ONE_EYE_TEST = "ALL_IN_ONE_EYE_TEST"
 
     const val USER_NAME = "USER_NAME"
     const val USER_EMAIL = "USER_EMAIL"
     const val USER_PHONE = "USER_PHONE"                                     // Long
     const val USER_AGE = "USER_AGE"                                        // Int
+
 
     // Eye Tests IDs
     const val MYOPIA = "MYOPIA"
@@ -40,6 +48,7 @@ object Constants {
 
     const val ML_MODEL = "ML_MODEL"
     const val LOGOUT = "LOGOUT"
+
 
     // Notification Settings Preferences
     const val NOTIFICATION_PREFERENCES = "NOTIFICATION_PREFERENCES"
