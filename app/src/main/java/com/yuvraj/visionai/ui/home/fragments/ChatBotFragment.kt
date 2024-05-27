82c1d3eb0b801434a78bea141603e2644feb34a0
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-05-27 22:31:40 +0530

    added chatbot logics

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/ChatBotFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/ChatBotFragment.kt
@@ -5,56 +5,92 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yuvraj.visionai.R
+import com.yuvraj.visionai.adapters.ChatMessages
+import com.yuvraj.visionai.databinding.FragmentHomeChatBotBinding
+import com.yuvraj.visionai.databinding.FragmentHomeStatisticsBinding
+import com.yuvraj.visionai.enums.ChatMessageSender.SENT_BY_BOT
+import com.yuvraj.visionai.enums.ChatMessageSender.SENT_BY_ME
+import com.yuvraj.visionai.model.ChatMessage
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import org.json.JSONException
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatBotFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ChatBotFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ChatBotFragment : Fragment(R.layout.fragment_home_chat_bot) {
+
+    private var _binding: FragmentHomeChatBotBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var messageList : MutableList<ChatMessage>
+    lateinit var messageAdapter : ChatMessages
+    val client = OkHttpClient()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Inflate the layout for this fragment
+        initViews(view)
+        clickableViews()
+    }
+
+
+    private fun initViews(view: View) {
+        _binding = FragmentHomeChatBotBinding.bind(view)
+
+        messageList = ArrayList()
+        messageAdapter = ChatMessages(messageList)
+
+        binding.apply{
+            recyclerView.adapter = messageAdapter
+            val layoutManger = LinearLayoutManager(requireActivity())
+            layoutManger.stackFromEnd = true
+            recyclerView.layoutManager = layoutManger
+        }
+
+    }
+
+    private fun clickableViews() {
+        binding.apply {
+            btnSend.setOnClickListener {
+                val question = inputMessage.text.toString().trim{ it <= ' '}
+                addToChat(question,SENT_BY_ME)
+                inputMessage.setText("")
+                callAPI(question)
+                welcomeText.visibility = View.GONE
+            }
+
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_chat_bot, container, false)
+    private fun addToChat(message: String, sentBy: String) {
+        requireActivity().runOnUiThread{
+            messageList.add(ChatMessage(message,sentBy))
+            messageAdapter.notifyDataSetChanged()
+            binding.recyclerView.smoothScrollToPosition(messageAdapter.itemCount)
+        }
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatBotFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatBotFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun addResponse(response:String?){
+        messageList.removeAt(messageList.size -1)
+        addToChat(response!!, SENT_BY_BOT)
+
+    }
+
+    private fun callAPI(question: String) {
+        // TODO: Implement API call
+
+    }
+    companion object{
+        val JSON : MediaType = "application/json; charset=utf-8".toMediaType()
     }
 }
+
