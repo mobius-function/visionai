68224ce459a33167ebf665f44b619f08e436dfa7
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-04 19:11:55 +0530

    added FireBase Repository

---
+++ app/src/main/java/com/yuvraj/visionai/repositories/FirebaseRepository.kt
@@ -0,0 +1,54 @@
+package com.yuvraj.visionai.repositories
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.yuvraj.visionai.model.EyeTestResult
+import com.yuvraj.visionai.model.UserPreferences
+
+class FirebaseRepository {
+
+    private val db = FirebaseFirestore.getInstance()
+
+    fun saveApiKey(apiKey: String) {
+        val apiKeyDoc = mapOf("apiKey" to apiKey)
+        db.collection("config").document("apiKey").set(apiKeyDoc)
+    }
+
+    fun getApiKey(callback: (String?) -> Unit) {
+        db.collection("config").document("apiKey").get()
+            .addOnSuccessListener { document ->
+                callback(document.getString("apiKey"))
+            }
+            .addOnFailureListener {
+                callback(null)
+            }
+    }
+
+    fun saveUserPreferences(userId: String, preferences: UserPreferences) {
+        db.collection("users").document(userId).set(preferences)
+    }
+
+    fun getUserPreferences(userId: String, callback: (UserPreferences?) -> Unit) {
+        db.collection("users").document(userId).get()
+            .addOnSuccessListener { document ->
+                callback(document.toObject(UserPreferences::class.java))
+            }
+            .addOnFailureListener {
+                callback(null)
+            }
+    }
+
+    fun saveEyeTest(userId: String, eyeTest: EyeTestResult) {
+        db.collection("users").document(userId).collection("eyeTests").add(eyeTest)
+    }
+
+    fun getEyeTests(userId: String, callback: (List<EyeTestResult>?) -> Unit) {
+        db.collection("users").document(userId).collection("eyeTests").get()
+            .addOnSuccessListener { documents ->
+                val eyeTests = documents.mapNotNull { it.toObject(EyeTestResult::class.java) }
+                callback(eyeTests)
+            }
+            .addOnFailureListener {
+                callback(null)
+            }
+    }
+}
