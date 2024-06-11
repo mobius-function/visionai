d537215d1c81c0f6e9c62abaca2aac5c4eed1659
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-11 12:29:19 +0530

    Added hilt base files

--- app/src/main/java/com/yuvraj/visionai/BaseApplication.kt
+++ app/src/main/java/com/yuvraj/visionai/BaseApplication.kt
@@ -1,4 +1,7 @@
 package com.yuvraj.visionai
 
-class BaseApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
