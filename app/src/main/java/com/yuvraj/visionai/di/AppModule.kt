4db64fc67d8a2fe56409927902a8c7e7cd4c2cc4
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-11 12:29:48 +0530

    Added AppModule for Dependency Injection

---
+++ app/src/main/java/com/yuvraj/visionai/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.yuvraj.visionai.di
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+}
