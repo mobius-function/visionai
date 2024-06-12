ad2c9978ecc155c2f5a389bb4241e3943ecf5fbc
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-12 23:58:52 +0530

    Completed Eye Test result paging adapter

--- app/src/main/java/com/yuvraj/visionai/adapters/EyeTestResultAdapter.kt
+++ app/src/main/java/com/yuvraj/visionai/adapters/EyeTestResultAdapter.kt
@@ -1,37 +1,41 @@
 package com.yuvraj.visionai.adapters
-//
-//import androidx.paging.PagingDataAdapter
-//import com.yuvraj.visionai.model.EyeTestResult
-//
-//
-//class EyeTestResultAdapter : PagingDataAdapter<EyeTestResult, EyeTestResultAdapter.EyeTestViewHolder>(EyeTestDiffCallback()) {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EyeTestViewHolder {
-//        val binding = ItemEyeTestBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//        return EyeTestViewHolder(binding)
-//    }
-//
-//    override fun onBindViewHolder(holder: EyeTestViewHolder, position: Int) {
-//        val eyeTest = getItem(position)
-//        if (eyeTest != null) {
-//            holder.bind(eyeTest)
-//        }
-//    }
-//
-//    class EyeTestViewHolder(private val binding: ItemEyeTestBinding) : RecyclerView.ViewHolder(binding.root) {
-////        fun bind(eyeTest: EyeTestResult) {
-////            binding.eyeTest = eyeTest
-////            binding.executePendingBindings()
-////        }
-//    }
-//
-//    class EyeTestDiffCallback : DiffUtil.ItemCallback<EyeTest>() {
-//        override fun areItemsTheSame(oldItem: EyeTest, newItem: EyeTest): Boolean {
-//            return oldItem.id == newItem.id
-//        }
-//
-//        override fun areContentsTheSame(oldItem: EyeTest, newItem: EyeTest): Boolean {
-//            return oldItem == newItem
-//        }
-//    }
-//}
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.yuvraj.visionai.databinding.ItemHomeStatisticsResultBinding
+import com.yuvraj.visionai.model.EyeTestResult
+
+
+class EyeTestResultAdapter : PagingDataAdapter<EyeTestResult, EyeTestResultAdapter.EyeTestViewHolder>(EyeTestDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EyeTestViewHolder {
+        val binding = ItemHomeStatisticsResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EyeTestViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EyeTestViewHolder, position: Int) {
+        val eyeTest = getItem(position)
+        if (eyeTest != null) {
+            holder.bind(eyeTest)
+        }
+    }
+
+    class EyeTestViewHolder(private val binding: ItemHomeStatisticsResultBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(eyeTest: EyeTestResult) {
+            binding.tvDateAndTime.text = eyeTest.id
+        }
+    }
+
+    class EyeTestDiffCallback : DiffUtil.ItemCallback<EyeTestResult>() {
+        override fun areItemsTheSame(oldItem: EyeTestResult, newItem: EyeTestResult): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: EyeTestResult, newItem: EyeTestResult): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
