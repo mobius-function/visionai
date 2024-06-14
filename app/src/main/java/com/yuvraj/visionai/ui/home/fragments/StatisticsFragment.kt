420d7b31ad6a9aa58874a7a45905b0bd4b9c8c4f
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-14 23:26:25 +0530

    completed UI for stats fragment fetching from firebase

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/StatisticsFragment.kt
@@ -3,14 +3,25 @@ package com.yuvraj.visionai.ui.home.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yuvraj.visionai.R
+import com.yuvraj.visionai.adapters.EyeTestResultAdapter
 import com.yuvraj.visionai.databinding.FragmentHomeStatisticsBinding
+import com.yuvraj.visionai.viewModel.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_home_statistics) {
 
     private var _binding: FragmentHomeStatisticsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: UserViewModel by viewModels()
+    private lateinit var adapter: EyeTestResultAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +33,13 @@ class StatisticsFragment : Fragment(R.layout.fragment_home_statistics) {
         // Inflate the layout for this fragment
         initViews(view)
         clickableViews()
+        setupRecyclerView()
+        observeData()
     }
 
 
     private fun initViews(view: View) {
+//        _binding = FragmentHomeStatisticsBinding.bind(view)
         _binding = FragmentHomeStatisticsBinding.bind(view)
     }
 
@@ -33,5 +47,24 @@ class StatisticsFragment : Fragment(R.layout.fragment_home_statistics) {
         binding.apply {
 
         }
+    }
+
+    private fun setupRecyclerView() {
+        adapter = EyeTestResultAdapter()
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
+    private fun observeData() {
+        lifecycleScope.launch {
+            viewModel.getEyeTestsPagingData().collectLatest { pagingData ->
+                adapter.submitData(pagingData)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
