9d340829892baffee74d3cd85a1a2e5d917e191e
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-18 23:37:09 +0530

    Updated InAppUpdate service

--- app/src/main/java/com/yuvraj/visionai/service/autoUpdater/InAppUpdate.kt
+++ app/src/main/java/com/yuvraj/visionai/service/autoUpdater/InAppUpdate.kt
@@ -1,99 +1,150 @@
 package com.yuvraj.visionai.service.autoUpdater
 
 import android.app.Activity
-import android.content.IntentSender.SendIntentException
-import android.view.View
+import android.content.IntentSender
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.common.IntentSenderForResultStarter
 import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
-import com.yuvraj.visionai.utils.ToastUtils.Companion.showToast
 
 
 class InAppUpdate(private val parentActivity: Activity) {
     private val appUpdateManager: AppUpdateManager?
     private val appUpdateType = AppUpdateType.FLEXIBLE
 
-    private var stateUpdatedListener =
-        InstallStateUpdatedListener { installState: InstallState ->
-            if (installState.installStatus() == InstallStatus.DOWNLOADED) {
-//                popupSnackBarForCompleteUpdate()
-                //show toast
-                Toast.makeText(parentActivity.applicationContext, "An update has just been downloaded.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
     init {
         appUpdateManager = AppUpdateManagerFactory.create(parentActivity)
     }
 
-    fun checkForAppUpdate() {
-        appUpdateManager!!.appUpdateInfo.addOnSuccessListener { info: AppUpdateInfo ->
-            val isUpdateAvailable =
-                info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-            val isUpdateAllowed = info.isUpdateTypeAllowed(appUpdateType)
-            if (isUpdateAvailable && isUpdateAllowed) {
+    fun checkUpdate() {
+        val appUpdateInfoTask = appUpdateManager?.appUpdateInfo
+        appUpdateInfoTask?.addOnSuccessListener { appUpdateInfo ->
+            // This example applies an flexible update. To apply a immediate update
+            // instead, pass in AppUpdateType.IMMEDIATE
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+            ) {
+                // Request the update
                 try {
-                    appUpdateManager.startUpdateFlowForResult(
-                        info,
-                        appUpdateType,
-                        parentActivity,
-                        MY_REQUEST_CODE
+                    appUpdateManager?.startUpdateFlowForResult(
+                        // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                        appUpdateInfo,
+                        // an activity result launcher registered via registerForActivityResult
+                        updateResultStarter,
+                        //pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
+                        // flexible updates.
+                        AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build(),
+                        // Include a request code to later monitor this update request.
+                        UPDATE_REQUEST_CODE
                     )
-                } catch (e: SendIntentException) {
-                    throw RuntimeException(e)
+                } catch (exception: IntentSender.SendIntentException) {
+                    Toast.makeText(parentActivity, "Something wrong went wrong!", Toast.LENGTH_SHORT).show()
                 }
+            } else if(appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)){
+                // Request the update
+                try {
+                    appUpdateManager?.startUpdateFlowForResult(
+                        // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                        appUpdateInfo,
+                        // an activity result launcher registered via registerForActivityResult
+                        updateResultStarter,
+                        //pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
+                        // flexible updates.
+                        AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+                        // Include a request code to later monitor this update request.
+                        UPDATE_REQUEST_CODE
+                    )
+                } catch (exception: IntentSender.SendIntentException) {
+                    Toast.makeText(parentActivity, "Something wrong went wrong!", Toast.LENGTH_SHORT).show()
+                }
+
+                } else {
+                Log.d("DebugInAppUpdate", "No Update available")
             }
         }
-        appUpdateManager.registerListener(stateUpdatedListener)
     }
 
-    fun onActivityResult(requestCode: Int, resultCode: Int) {
-        if (requestCode == MY_REQUEST_CODE) {
-            if (resultCode == RESULT_CANCELED) {
-                showToast(parentActivity.applicationContext, "Update canceled by user")
-                Toast.makeText(parentActivity.applicationContext, "Update canceled by user", Toast.LENGTH_SHORT).show()
-            } else if (resultCode != AppCompatActivity.RESULT_OK) {
-                checkForAppUpdate()
+    private val updateLauncher = AppCompatActivity().registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        // handle callback
+        if (result.data == null) return@registerForActivityResult
+        if (result.resultCode == UPDATE_REQUEST_CODE) {
+            Toast.makeText(parentActivity, "Downloading stated", Toast.LENGTH_SHORT).show()
+
+            if (result.resultCode != Activity.RESULT_OK) {
+                Toast.makeText(parentActivity, "Downloading failed", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-//    private fun popupSnackBarForCompleteUpdate() {
-//        Snackbar.make(
-//            parentActivity.findViewById<View>(R.id.postsFrameLayout),
-//            "An update has just been downloaded.",
-//            Snackbar.LENGTH_INDEFINITE
-//        ).setAction(
-//            "RESTART"
-//        ) { view: View? ->
-//            appUpdateManager?.completeUpdate()
-//        }.show()
-//    }
+    private val updateResultStarter =
+        IntentSenderForResultStarter { intent, _, fillInIntent, flagsMask, flagsValues, _, _ ->
+            val request = IntentSenderRequest.Builder(intent)
+                .setFillInIntent(fillInIntent)
+                .setFlags(flagsValues, flagsMask)
+                .build()
+            // launch updateLauncher
+            updateLauncher.launch(request)
+        }
+
+    private val listener = InstallStateUpdatedListener { state ->
+        // (Optional) Provide a download progress bar.
+        // TODO: Implement progress bar (Optional)
+        if (state.installStatus() == InstallStatus.DOWNLOADING) {
+            val bytesDownloaded = state.bytesDownloaded()
+            val totalBytesToDownload = state.totalBytesToDownload()
+            val progress = (bytesDownloaded * 100 / totalBytesToDownload).toInt()
+            // Show update progress bar.
+        }
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            Snackbar.make(
+                parentActivity.findViewById(android.R.id.content),
+                "New app is ready",
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction("Restart") {
+                appUpdateManager?.completeUpdate()
+            }.show()
+        }
+        // Log state or install the update.
+    }
+
+    fun onStart() {
+        appUpdateManager?.registerListener(listener)
+    }
 
     fun onResume() {
-        appUpdateManager?.appUpdateInfo?.addOnSuccessListener { info: AppUpdateInfo ->
-            if (info.installStatus() == InstallStatus.DOWNLOADED) {
-//                popupSnackBarForCompleteUpdate()
-                //show toast
-                Toast.makeText(parentActivity.applicationContext, "An update has just been downloaded.", Toast.LENGTH_SHORT).show()
+        appUpdateManager?.completeUpdate()
+
+        appUpdateManager?.appUpdateInfo?.addOnSuccessListener{ appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability()
+                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+            ) {
+                // If an in-app update is already running, resume the update.
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    updateLauncher,
+                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build())
             }
         }
     }
 
     fun onDestroy() {
-        appUpdateManager?.unregisterListener(stateUpdatedListener)
+        appUpdateManager?.unregisterListener(listener)
     }
 
     companion object{
-        const val RESULT_CANCELED = 0
-        const val MY_REQUEST_CODE = 500
+        const val UPDATE_REQUEST_CODE = 303
     }
 }
