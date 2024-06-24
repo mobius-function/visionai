88d9959b5a24b478880d066b16ebbfe49b9bdce0
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-06-24 22:27:02 +0530

    Updated data classes

--- app/src/main/java/com/yuvraj/visionai/model/EyeTestResult.kt
+++ app/src/main/java/com/yuvraj/visionai/model/EyeTestResult.kt
@@ -1,14 +1,14 @@
 package com.yuvraj.visionai.model
 
 data class EyeTestResult(
-    val id: String = "1/1/2021 12:00:00",
-    val plusPowerRightEye: Float = 0f,
-    val plusPowerLeftEye: Float = 0f,
-    val minusPowerRightEye: Float = 0f,
-    val minusPowerLeftEye: Float = 0f,
-    val astigmatismResult: Boolean = false,
-    val dryLeftEyeResult: Boolean = false,
-    val dryRightEyeResult: Boolean = false,
-    val jaundiceResult: Boolean = false
+    val id: String? = "",
+    val plusPowerRightEye: Float? = 0f,
+    val plusPowerLeftEye: Float? = 0f,
+    val minusPowerRightEye: Float? = 0f,
+    val minusPowerLeftEye: Float? = 0f,
+    val astigmatismResult: Boolean? = false,
+    val dryLeftEyeResult: Boolean? = false,
+    val dryRightEyeResult: Boolean? = false,
+    val jaundiceResult: Boolean? = false
 )
 
--- app/src/main/java/com/yuvraj/visionai/model/UserPreferences.kt
+++ app/src/main/java/com/yuvraj/visionai/model/UserPreferences.kt
@@ -1,9 +1,11 @@
 package com.yuvraj.visionai.model
 
 data class UserPreferences(
-    val name: String = "",
-    val phoneNumber: String = "",
-    val age: Int = 0,
-    val gender: String = "",
-    val email: String = ""
-)
+    val name: String? = "",
+    val phoneNumber: String? = "",
+    val age: Int? = 0,
+    val gender: String? = "",
+    val email: String? = ""
+) {
+    constructor() : this("", "", 0, "", "")
+}
