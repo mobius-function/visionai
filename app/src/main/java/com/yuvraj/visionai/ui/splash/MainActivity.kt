abe30784c0a22f4d4b84e81e262c98d0b7d03b14
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-07-19 00:32:34 +0530

    updated activity

--- app/src/main/java/com/yuvraj/visionai/ui/splash/MainActivity.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/splash/MainActivity.kt
@@ -2,11 +2,13 @@ package com.yuvraj.visionai.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.yuvraj.visionai.R
 import com.yuvraj.visionai.databinding.UiSplashScreenBinding
 import com.yuvraj.visionai.ui.onBoarding.MainActivity
+import com.yuvraj.visionai.utils.ScreenUtils.hideSystemUI
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,15 +18,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         initViews()
         setContentView(binding?.root)
-    }
 
-    fun initViews() {
-        binding = UiSplashScreenBinding.inflate(layoutInflater,null,false)
+        hideSystemUI()
 
         // pause for 8 seconds
         Thread.sleep(8000)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+//        val intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+
+        binding?.apply {
+            btnStart.visibility = View.VISIBLE
+            btnStart.setOnClickListener {
+                val intent = Intent(this@MainActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    fun initViews() {
+        binding = UiSplashScreenBinding.inflate(layoutInflater,null,false)
     }
 }
