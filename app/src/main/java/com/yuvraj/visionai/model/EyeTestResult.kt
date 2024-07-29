a5c28b746226a0ac29b8f18d84d2df5ca2313294
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-07-29 19:07:23 +0530

    Debug for sharing data via viewModel

--- app/src/main/java/com/yuvraj/visionai/model/EyeTestResult.kt
+++ app/src/main/java/com/yuvraj/visionai/model/EyeTestResult.kt
@@ -1,16 +1,19 @@
 package com.yuvraj.visionai.model
 
+import java.util.Calendar
+
 data class EyeTestResult(
-    val id: String? = "",
-    val plusPowerRightEye: Float? = 0f,
-    val plusPowerLeftEye: Float? = 0f,
-    val minusPowerRightEye: Float? = 0f,
-    val minusPowerLeftEye: Float? = 0f,
-    val astigmatismResult: Boolean? = false,
-    val dryLeftEyeResult: Boolean? = false,
-    val dryRightEyeResult: Boolean? = false,
-    val jaundiceResult: Boolean? = false
+    var id: String? = "",
+    var plusPowerRightEye: Float? = 0f,
+    var plusPowerLeftEye: Float? = 0f,
+    var minusPowerRightEye: Float? = 0f,
+    var minusPowerLeftEye: Float? = 0f,
+    var astigmatismResult: Boolean? = false,
+    var dryLeftEyeResult: Boolean? = false,
+    var dryRightEyeResult: Boolean? = false,
+    var jaundiceResult: Boolean? = false
 ) {
     // No-argument constructor is implicitly added by the Kotlin compiler
+    constructor() : this(Calendar.getInstance().time.toString(), 0f, 0f, 0f, 0f, false, false, false, false)
 }
 
