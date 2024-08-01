a4c8b1f4bd32b8fd1a0670e68c10a5deaa6968a9
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-08-02 00:44:10 +0530

    Improved Home UX

--- app/src/main/java/com/yuvraj/visionai/ui/home/MainActivity.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.yuvraj.visionai.ui.home
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -154,6 +159,22 @@ class MainActivity : AppCompatActivity() {
                 tvToolbarShortName.text = "GU"
             }
         }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+                    imm?.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 
     override fun onResume() {
