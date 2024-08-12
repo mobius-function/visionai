a297df4b147512d08a323314f0fb4922f24a3577
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-08-12 23:26:24 +0530

    Added new analytics Model

---
+++ app/src/main/java/com/yuvraj/visionai/analytics/PastResultsModel.kt
@@ -0,0 +1,8 @@
+package com.yuvraj.visionai.analytics
+
+data class PastResultsModel (
+    val pastLeftEyeMyopia : Float,
+    val pastRightEyeMyopia : Float,
+    val pastLeftEyeHyperopia : Float,
+    val pastRightEyeHyperopia : Float
+)
