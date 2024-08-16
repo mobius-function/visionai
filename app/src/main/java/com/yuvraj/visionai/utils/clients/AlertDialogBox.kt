61d9c098a59430610af6e1ef73261ea7e11d5041
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-08-17 01:06:08 +0530

    updated alertDiaglog with new features like restore default and get Stored focal length

--- app/src/main/java/com/yuvraj/visionai/utils/clients/AlertDialogBox.kt
+++ app/src/main/java/com/yuvraj/visionai/utils/clients/AlertDialogBox.kt
@@ -4,11 +4,13 @@ import android.app.Activity
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.yuvraj.visionai.R
+import com.yuvraj.visionai.utils.PowerAlgorithm.Companion.getDefaultFocalLength
+import com.yuvraj.visionai.utils.PowerAlgorithm.Companion.getFrontCameraFocalLength
 import com.yuvraj.visionai.utils.helpers.SharedPreferencesHelper.getFocalLength
 import com.yuvraj.visionai.utils.helpers.SharedPreferencesHelper.setFocalLength
 
@@ -49,6 +51,17 @@ class AlertDialogBox {
             // Set up the buttons
             val etInput : TextInputEditText = viewInflated.findViewById(R.id.etInput)
             val tilInput : TextInputLayout = viewInflated.findViewById(R.id.tilInput)
+
+            val btnRestoreDefault : TextView = viewInflated.findViewById(R.id.btnRestoreDefault)
+            val btnGetStoredFocalLength : TextView = viewInflated.findViewById(R.id.btnGetStoredFocalLength)
+
+            btnRestoreDefault.setOnClickListener {
+                etInput.setText(getDefaultFocalLength().toString())
+            }
+
+            btnGetStoredFocalLength.setOnClickListener {
+                etInput.setText(getFrontCameraFocalLength().toString())
+            }
 
             tilInput.hint = "Enter Focal Length"
 
