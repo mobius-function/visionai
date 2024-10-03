46590e493487b0b721e80d568832df2c7c6b98d0
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-10-03 19:18:46 +0530

    Implemented Dagger Hilt for chat Messages

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/ChatBotFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/ChatBotFragment.kt
@@ -1,7 +1,6 @@
 package com.yuvraj.visionai.ui.home.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
