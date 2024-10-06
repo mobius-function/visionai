a35ea9ee6db64e04852578276c87ff6dd606ef04
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-10-06 15:20:10 +0530

    Added ViewModel to the ChatBotFragment

---
+++ app/src/main/java/com/yuvraj/visionai/ui/home/viewModel/ChatBotViewModel.kt
@@ -0,0 +1,61 @@
+package com.yuvraj.visionai.ui.home.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.yuvraj.visionai.db.dao.ChatMessageDao
+import com.yuvraj.visionai.db.entities.ChatMessageEntity
+import androidx.lifecycle.viewModelScope
+import com.yuvraj.visionai.enums.ChatMessageSender
+import com.yuvraj.visionai.model.ChatMessage
+import com.yuvraj.visionai.repositories.ChatResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatBotViewModel @Inject constructor(
+    private val chatMessageDao: ChatMessageDao
+) : ViewModel() {
+
+    val messageList: MutableList<ChatMessage> = mutableListOf()
+
+
+    fun loadPreviousMessages(onLoaded: () -> Unit) {
+        viewModelScope.launch {
+            val messages = chatMessageDao.getAllMessages()
+            messages.forEach { entity ->
+                messageList.add(
+                    ChatMessage(
+                        entity.message,
+                        if (entity.sentBy == "SENT_BY_ME") ChatMessageSender.SENT_BY_ME else ChatMessageSender.SENT_BY_BOT
+                    )
+                )
+            }
+            onLoaded()
+        }
+    }
+
+    fun addMessageToChat(message: String, sentBy: ChatMessageSender, onAdded: () -> Unit) {
+        viewModelScope.launch {
+            messageList.add(ChatMessage(message, sentBy))
+
+            if(sentBy == ChatMessageSender.SENT_BY_BOT && message == "Typing...") {
+                // Dont log
+            } else {
+                chatMessageDao.insertMessage(
+                    ChatMessageEntity(
+                        message = message,
+                        sentBy = sentBy.name
+                    )
+                )
+            }
+
+            onAdded()
+        }
+    }
+
+    fun getResponseFromAPI(question: String, onResponse: (String) -> Unit) {
+        ChatResponse.getResFun(question) { response ->
+            onResponse(response)
+        }
+    }
+}
