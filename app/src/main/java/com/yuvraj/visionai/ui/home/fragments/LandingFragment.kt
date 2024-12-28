890291c62e43bf30f43f85289d788c1381aa79a0
Yuvraj Kumar <img_2021066@iiitm.ac.in>
2024-12-28 23:28:57 +0530

    Updated Landing Fragment with a Notification Alert View

--- app/src/main/java/com/yuvraj/visionai/ui/home/fragments/LandingFragment.kt
+++ app/src/main/java/com/yuvraj/visionai/ui/home/fragments/LandingFragment.kt
@@ -27,6 +27,7 @@ import com.yuvraj.visionai.utils.Constants.MYOPIA
 import com.yuvraj.visionai.utils.Constants.REQUIRED_PERMISSIONS
 import com.yuvraj.visionai.utils.Constants.USER_PROFILE
 import com.yuvraj.visionai.utils.PowerAlgorithm.Companion.calculateEyeHealthScore
+import com.yuvraj.visionai.utils.helpers.SharedPreferencesHelper.getLastEyeTestDate
 import com.yuvraj.visionai.utils.helpers.SharedPreferencesHelper.isDebugMode
 import com.yuvraj.visionai.utils.helpers.SharedPreferencesHelper.setDebugMode
 import com.yuvraj.visionai.viewModel.UserViewModel
@@ -53,38 +54,25 @@ class LandingFragment : Fragment(R.layout.fragment_home_landing) {
         initViews(view)
         checkForPermission()
         clickableViews()
+        setAlertView(eyeHealthScore > 60)
     }
 
     private fun initViews(view: View) {
         _binding = FragmentHomeLandingBinding.bind(view)
         _eyeHealthScore = requireActivity().calculateEyeHealthScore()
 
+        setEyeHealthScore(eyeHealthScore)
+
         val tests = getEyeTestMenuList()
-
         val adapter = EyeTestsList(tests, this::onListItemClick)
-        binding.testsRecyclerView.adapter = adapter
 
+        binding.testsRecyclerView.adapter = adapter
         binding.testsRecyclerView.addItemDecoration(
             DividerItemDecoration(requireActivity(), LinearLayoutManager.VERTICAL)
         )
 
         viewModel.apply {
             clearTestResults()
-        }
-
-        binding.apply {
-            circularProgressBar.apply {
-                progress = 0f
-                setProgressWithAnimation(eyeHealthScore * 0.8f, 1000)
-            }
-
-            waterWaveView.apply {
-                progress = eyeHealthScore.toInt()
-                setAnimationSpeed(50)
-                setBehindWaveColor("#254015".toColorInt())
-                setFrontWaveColor("#1d4009".toColorInt())
-                startAnimation()
-            }
         }
     }
 
@@ -148,20 +136,78 @@ class LandingFragment : Fragment(R.layout.fragment_home_landing) {
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                // continue
-            } else {
-                Toast.makeText(requireActivity(),
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
+    private fun setAlertView(isGoodScore : Boolean) {
+        binding.apply{
+            // No last test recorded
+            if(requireActivity().getLastEyeTestDate() == "") {
+                setEyeHealthScore(0f)
+                alertView.apply {
+                    layout.background = requireActivity().getDrawable(R.drawable.bg_alert_red)
+                    tvTitle.text = "No Test Recorded!"
+                    tvContent.text = "You have not taken any eye test yet." +
+                            "\n\nWe recommend you to take a full eye test for updated result."
 
-                requireActivity().finish()
+                    btnTest.apply {
+                        setBackgroundColor(requireActivity().getColor(R.color.red_button))
+                        visibility = View.VISIBLE
+                        setOnClickListener {
+                            findNavController().navigate(R.id.action_landingFragment_to_eyeTestingFragment)
+                        }
+                    }
+                }
+                return
+            }
+
+
+            else if(isGoodScore) {
+                alertView.apply {
+                    layout.background = requireActivity().getDrawable(R.drawable.bg_alert_green)
+                    tvTitle.text = "Good Eye Health!"
+                    tvContent.text = "Your eye health score is ${eyeHealthScore.toInt()}!" +
+                            "\n\nKeep it up! You are doing great. Continue to take care of your eyes."
+
+                    btnTest.apply {
+                        setBackgroundColor("#c0faa2".toColorInt())
+                        visibility = View.GONE
+                        setOnClickListener {
+                            // do nothing
+                        }
+                    }
+                }
+            } else {
+                alertView.apply {
+                    layout.background = requireActivity().getDrawable(R.drawable.bg_alert_red)
+                    tvTitle.text = "Poor Eye Health!"
+                    tvContent.text = "Your eye health score is ${eyeHealthScore.toInt()}!" +
+                            "\n\nYou need to take care of your eyes. We recommend you to take a full eye test for further updated result."
+
+                    btnTest.apply {
+                        setBackgroundColor(requireActivity().getColor(R.color.red_button))
+                        visibility = View.VISIBLE
+                        setOnClickListener {
+                            findNavController().navigate(R.id.action_landingFragment_to_eyeTestingFragment)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    private fun setEyeHealthScore(score: Float) {
+        binding.apply {
+            circularProgressBar.apply {
+                progress = 0f
+                setProgressWithAnimation(score * 0.8f, 1000)
+            }
+
+            waterWaveView.apply {
+                progress = score.toInt()
+                setAnimationSpeed(50)
+                setBehindWaveColor("#254015".toColorInt())
+                setFrontWaveColor("#1d4009".toColorInt())
+                startAnimation()
             }
         }
     }
